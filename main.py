dfce004bbad6614b45343a7c998828191946550e
Matt Ferraro <matt@planet.com>
2015-12-17 04:30:01 -0500

    library-ized the functions. Added backprojection

--- main.py
+++ main.py
@@ -1,48 +1,21 @@
 import cv2
-import numpy as np
 import matplotlib.pyplot as plt
+import radon
+import argparse
 
 
-def prepare_image():
-    image = cv2.imread('cat.jpg')
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    small_image = cv2.resize(
-        gray_image, None, fx=.5, fy=.5, interpolation=cv2.INTER_AREA)
-    height, width = small_image.shape
-    circle = np.zeros((height, width))
-    cv2.circle(circle, (width/2, height/2), height/2, 1, thickness=-1)
-    masked_data = small_image * circle
-    extra_pixels = width - height
-    cropped = masked_data[0:height, extra_pixels / 2:width - extra_pixels/2]
-    cv2.imwrite('prepared.png', cropped)
+def main(img_name):
+    # radon.prepare_image('cat.jpg', 'prepared.png')
+    image = cv2.imread(img_name, 0)
+    sinogram = radon.radon_transform(image)
+    reconstructed = radon.back_project(sinogram)
 
-
-def main():
-    prepare_image()
-    image = cv2.imread('dot.png', 0)
-    rows, cols = image.shape
-    # cv2.imshow('prepared', image)
-
-    all_projected = {}
-
-    angles = range(0, 360, 1)
-    for i in angles:
-        M = cv2.getRotationMatrix2D((cols/2, rows/2), i, 1)
-        rotated = cv2.warpAffine(image, M, (cols, rows))
-        projected = rotated.sum(axis=0)
-        all_projected[i] = projected
-        # cv2.imshow('rotated {}'.format(i), rotated)
-        # plt.plot(projected)
-
-    # Build sinogram!
-    height = len(all_projected.keys())
-    width = len(all_projected[0])
-    sinogram = np.zeros((height, width))
-    for index, angle in enumerate(angles):
-        sinogram[index] = all_projected[angle]
-
-    plt.imshow(sinogram, cmap='gray', interpolation='bicubic')
+    plt.imshow(reconstructed, cmap='gray', interpolation='bicubic')
     plt.show()
 
+
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image', default='prepared.png')
+    args = parser.parse_args()
+    main(args.image)
---
+++ radon.py
@@ -0,0 +1,57 @@
+import cv2
+import numpy as np
+
+
+def prepare_image(input_name, output_name):
+    '''
+    Given an input image, prepare it for use in radon related stuff.
+    That means:
+        -cast to grayscale
+        -resize to be small
+        -mask out all but a central circle
+        -save as output file name
+    '''
+    image = cv2.imread(input_name)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    small_image = cv2.resize(
+        gray_image, None, fx=.5, fy=.5, interpolation=cv2.INTER_AREA)
+    height, width = small_image.shape
+    circle = np.zeros((height, width))
+    cv2.circle(circle, (width/2, height/2), height/2, 1, thickness=-1)
+    masked_data = small_image * circle
+    extra_pixels = width - height
+    cropped = masked_data[0:height, extra_pixels / 2:width - extra_pixels/2]
+    cv2.imwrite(output_name, cropped)
+
+
+def radon_transform(image):
+    '''
+    Perform the radon transform on an image, returning the sinogram
+    '''
+    rows, cols = image.shape
+    angles = range(0, 360, 1)
+    height = len(angles)
+    width = cols
+    sinogram = np.zeros((height, width))
+    for index, alpha in enumerate(angles):
+        M = cv2.getRotationMatrix2D((cols/2, rows/2), alpha, 1)
+        rotated = cv2.warpAffine(image, M, (cols, rows))
+        sinogram[index] = rotated.sum(axis=0)
+    return sinogram
+
+
+def back_project(sinogram):
+    '''
+    Given a sinogram, return the back-projection of it
+    '''
+    rotation_angle = 360 / len(sinogram)
+    width = height = len(sinogram[0])
+    reconstructed = np.zeros((width, height))
+    M = cv2.getRotationMatrix2D((width/2, height/2), rotation_angle, 1)
+    for index, projection in enumerate(sinogram):
+        small_projection = projection / height
+        for row in reconstructed:
+            row += small_projection
+        reconstructed = cv2.warpAffine(reconstructed, M, (width, height))
+
+    return reconstructed
